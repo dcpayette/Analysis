4faf590b5db25fbbd73dd530950d69175db537d4
dcpayette <payette@jlab.org>
2018-08-01 10:58:32 -0400

    Update wanalysis.groovy

--- wanalysis.groovy
+++ wanalysis.groovy
@@ -51,52 +51,57 @@ double vzmax = 0;
 int counter = 0;
 
 while (reader.hasEvent()) {
-      DataEvent event = reader.getNextEvent();
-      if (event.hasBank("REC::Particle")) {
-      	 DataBank bank_rec = event.getBank("REC::Particle");
-	 //counter++;
-	 //if(counter > 100){break;}
-	 for (int k = 0; k < bank_rec.rows(); k++) {
-	     int pid = bank_rec.getInt("pid", k);
-	     float px = bank_rec.getFloat("px", k);
-	     float py = bank_rec.getFloat("py", k);								
-	     float pz = bank_rec.getFloat("pz", k);
-	     float beta = bank_rec.getFloat("beta", k);
-	     float mom = (float) Math.sqrt(px * px + py * py + pz * pz);
-	     double phi = Math.atan2((double) py,(double) px);
-	     double theta = Math.acos((double) pz/(double) mom);
-	     float vz = bank_rec.getFloat("vz", k);	     
+	DataEvent event = reader.getNextEvent();
+      	if (event.hasBank("REC::Particle")) {
+      	 	DataBank bank_rec = event.getBank("REC::Particle");
+	 		//counter++;
+	 		//if(counter > 100){break;}
+		for (int k = 0; k < bank_rec.rows(); k++) {
+			int pid = bank_rec.getInt("pid", k);
+			float px = bank_rec.getFloat("px", k);
+			float py = bank_rec.getFloat("py", k);								
+			float pz = bank_rec.getFloat("pz", k);
+			float beta = bank_rec.getFloat("beta", k);
+			float mom = (float) Math.sqrt(px * px + py * py + pz * pz);
+			double phi = Math.atan2((double) py,(double) px);
+			double theta = Math.acos((double) pz/(double) mom);
+			float vz = bank_rec.getFloat("vz", k);	     
 
-	     if (pid != 11) continue;
-	     	momentum.fill(mom);
-	     	Vector3 e_vec_3 = new Vector3(px, py, pz);
-		LorentzVector e_vec_prime = new LorentzVector();
-		e_vec_prime.setVectM(e_vec_3, e_mass);
-		LorentzVector q_vec = new LorentzVector();
-		q_vec.copy(e_vec);
-		q_vec.sub(e_vec_prime);
-		double Q2 = -q_vec.mass2();
-		Q2_hist.fill(Q2);
-		LorentzVector temp = new LorentzVector();
-		temp.copy(p_vec);
-		temp.add(q_vec);
-		double W = temp.mass();
-		W_hist.fill(W);
-		W_vs_Q2.fill(Q2,W);
-		if(e_vec_prime.e()>emax){emax = e_vec_prime.e();}
-		if(theta > thetamax){thetamax = theta;}
-		if(phi > phimax){phimax = phi;}
-		if(vz > vzmax){vzmax = vz;}
-		E_vs_Theta.fill(theta,e_vec_prime.e());
-		z_vs_Theta.fill(theta,vz);
-		Phi_vs_Theta.fill(theta,phi);
+			if (pid != 11) continue;
+			
+			momentum.fill(mom);
+			
+			Vector3 e_vec_3 = new Vector3(px, py, pz); //3 vector e'
+			LorentzVector e_vec_prime = new LorentzVector(); //4 vector e'
+			e_vec_prime.setVectM(e_vec_3, e_mass);
+			
+			LorentzVector q_vec = new LorentzVector(); //4 vector q
+			q_vec.copy(e_vec); //e - e'
+			q_vec.sub(e_vec_prime);
+			double Q2 = -q_vec.mass2(); //-q^2
+			Q2_hist.fill(Q2);
+			
+			LorentzVector w_vec = new LorentzVector(); //4 vector used to calculate W
+			w_vec.copy(p_vec); //p-q
+			w_vec.add(q_vec);
+			double W = w_vec.mass(); 
+			W_hist.fill(W);
+			W_vs_Q2.fill(Q2,W);
+			
+			if(e_vec_prime.e()>emax){emax = e_vec_prime.e();} //calculate max values of each param
+			if(theta > thetamax){thetamax = theta;}
+			if(phi > phimax){phimax = phi;}
+			if(vz > vzmax){vzmax = vz;}
+			
+			E_vs_Theta.fill(theta,e_vec_prime.e());
+			z_vs_Theta.fill(theta,vz);
+			Phi_vs_Theta.fill(theta,phi);
 		}
 	}
 }
-//E_vs_Theta.set(500,-thetamax,thetamax,500,0,emax);
-//z_vs_Theta.set(500,-thetamax,thetamax,500,-vzmax,vzmax);
-//Phi_vs_Theta.set(500,-thetamax,thetamax,500,-phimax,phimax);
+
 System.out.println(emax + " " + thetamax + " " + phimax + " " + vzmax);
+
 TCanvas can = new TCanvas("can", 800, 600);
 can.draw(W_vs_Q2);
 can.save("W_vs_Q2.png");
