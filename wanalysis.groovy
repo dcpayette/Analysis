e543563ed2b97dce7d1aff293cb53a941a862eb5
David Payette <payette@jlab.org>
2018-08-14 09:32:31 -0400

    bug fix

--- wanalysis.groovy
+++ wanalysis.groovy
@@ -57,7 +57,7 @@ H1F Cal_lu = new H1F("Cal_lu", "Cal_lu", 500, 0, 450);
 H1F Cal_lv = new H1F("Cal_lv", "Cal_lv", 500, 0, 450); 
 H1F Cal_lw = new H1F("Cal_lw", "Cal_lw", 500, 0, 450); 
 
-H2F Cal_y_vs_x = new H2F("Cal_y_vs_x", "Cal_y_vs_x", 500, -100, 100, 500, -100, 100);
+H2F Cal_y_vs_x = new H2F("Cal_y_vs_x", "Cal_y_vs_x", 500, -1000, 1000, 500, -1000, 1000);
 Cal_y_vs_x.setTitleX("X (cm)");
 Cal_y_vs_x.setTitleY("Y (cm)");
 	
@@ -198,8 +198,8 @@ can8.draw(Cal_y_vs_x_precut);
 can8.save("Calyvxprecut.png");
 
 TCanvas can12 = new TCanvas("can", 800,600);
-can8.draw(Cal_y_vs_x);
-can8.save("Calyvx.png");
+can12.draw(Cal_y_vs_x);
+can12.save("Calyvx.png");
 	   
 TCanvas can9 = new TCanvas("can", 800,600);
 can9.draw(Cal_lu);
